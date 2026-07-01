c60d113e96960003b829af1c21f4f4ddf10b82b6
sasi <sasi@adsstudio.in>
2026-07-01 15:10:16 +0530

    Remove packages page

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,6 @@ export default defineConfig({
             "/contact",
             "/faq",
             "/gallery",
-            "/packages",
             "/real-weddings",
             "/services",
             "/testimonials",
